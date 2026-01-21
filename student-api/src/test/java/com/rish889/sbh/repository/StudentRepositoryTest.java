55e826892641a8cedb0aff18c02586f377c3a3a0
Rishabh Garcha <rish889@gmail.com>
2026-01-21 11:39:33 +0530

    rishCommit

---
+++ student-api/src/test/java/com/rish889/sbh/repository/StudentRepositoryTest.java
@@ -0,0 +1,10 @@
+package com.rish889.sbh.repository;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class StudentRepositoryTest {
+    // Your tests here
+}
