99dedefa2cbeedd3b6498954ecdf09078bc65b31
Rishabh Garcha <rish889@gmail.com>
2026-01-21 09:31:34 +0530

    rishCommit

--- student-api/src/main/java/com/rish889/sbh/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.rish889.sbh.repository;
-
-import com.rish889.sbh.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
-
